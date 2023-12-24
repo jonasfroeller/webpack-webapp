026b077f800e5d7a517d931e8b2ecd1a69d32589
jonasfroeller <jonasfroeller@users.noreply.github.com>
2023-12-24 11:43:01 +0000

    deploy: f68ff78d239c74f996dddb2a6696afa287a03f94

--- lib/script/pages/index.d.ts
+++ lib/script/pages/index.d.ts
@@ -1,3 +1,6 @@
 import '../../style/main.css';
 import '../../style/style.scss';
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/themes/dark.css';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 //# sourceMappingURL=index.d.ts.map
